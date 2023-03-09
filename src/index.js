192ede30aaf659bac44a5f9912b5762d3ffe9f7d
Danish Sharma <danishsharma13255@gmail.com>
2023-03-09 22:58:25 +0530

    Started with Editor

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Home from "./pages/home/Home";
 import Registration from "./pages/registration/Registration";
 import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
-import OutputWindow from "./components/outputwindow/OutputWindow";
+import Editor from "./pages/editor/Editor";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/editor",
-    element: <OutputWindow />,
+    element: <Editor />,
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/pages/editor/Editor.jsx
+++ src/pages/editor/Editor.jsx
@@ -1,4 +1,20 @@
 import React from "react";
+import CodeEditorWindow from "./CodeEditorWindow";
+import axios from "axios";
+import { classnames } from "../utils/general";
+import { languageOptions } from "../constants/languageOptions";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { defineTheme } from "../lib/defineTheme";
+import useKeyPress from "../hooks/useKeyPress";
+import Footer from "./Footer";
+import OutputWindow from "./OutputWindow";
+import CustomInput from "./CustomInput";
+import OutputDetails from "./OutputDetails";
+import ThemeDropdown from "./ThemeDropdown";
+import LanguagesDropdown from "./LanguagesDropdown";
 
 const Editor = () => {
   return <div>hello</div>;
