5279929d8b968b259657e73d8ca18be1097b8bf7
Danish Sharma <danishsharma13255@gmail.com>
2023-03-14 15:32:10 +0530

    profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -6,7 +6,13 @@ const Profile = () => {
     <div className="main">
       <Header />
       <div className="profile_container">
-        <div className="box1">Hello</div>
+        <div className="box1">
+          <div className="profile-card">
+            <h1>Name</h1>
+            <h3>About</h3>
+            <h3>about 2</h3>
+          </div>
+        </div>
         <div className="box2">Box2</div>
         <div className="box3">BOX3</div>
       </div>
