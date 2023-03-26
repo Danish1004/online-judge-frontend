7fbdbbeee5e9a19f9c73a01962f87596586b7f81
Danish Sharma <danishsharma13255@gmail.com>
2023-03-26 16:02:32 +0530

    profile page changes

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Profile.css";
 import Header from "../../components/header/Header";
-import "./Profile.css";
 import Footer from "../../components/footer/Footer";
 // import user from "../../images/user1.png";
 // import edit from "../../images/edit.png";
@@ -13,8 +12,8 @@ const Profile = () => {
       </div>
       <div className="main-box">
         <div className="profile-content">
-          <div className="info-card">
-            <h3>General Information</h3>
+          <form className="info-card">
+            <h2>General Information</h2>
             <div className="heading-1">
               <div className="sub-head">
                 <h5 className="sub-info">First Name</h5>
@@ -27,15 +26,60 @@ const Profile = () => {
             </div>
             <div className="heading-1">
               <div className="sub-head">
-                <h5>Email</h5>
+                <h5 className="sub-info">Email</h5>
                 <input type="email"></input>
               </div>
               <div className="sub-head">
-                <h5>Phone Number</h5>
+                <h5 className="sub-info">Phone Number</h5>
                 <input type="tel"></input>
               </div>
             </div>
-          </div>
+            <div className="heading-1">
+              <div className="sub-head">
+                <h5 className="sub-info">Gender</h5>
+                <input type="text"></input>
+              </div>
+              <div className="sub-head">
+                <h5 className="sub-info">Birth Date</h5>
+                <input type="date"></input>
+              </div>
+            </div>
+            <div className="address-col">
+              <h2>Address</h2>
+              <div className="heading-1">
+                <div className="sub-head2">
+                  <h5 className="sub-info">Address</h5>
+                  <input type="text"></input>
+                </div>
+                <div className="sub-head3">
+                  <h5 className="sub-info">Flat/House No.</h5>
+                  <input type="text"></input>
+                </div>
+              </div>
+              <div className="heading-1">
+                <div className="sub-head4">
+                  <h5 className="sub-info">City</h5>
+                  <input type="text"></input>
+                </div>
+                <div className="sub-head4">
+                  <h5 className="sub-info">State</h5>
+                  <input type="text"></input>
+                </div>
+                <div className="sub-head4">
+                  <h5 className="sub-info">Pincode.</h5>
+                  <input type="postal"></input>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <input
+                type="submit"
+                // onClick={handleSubmit}
+                className="btn-2"
+                value="Save All"
+              ></input>
+            </div>
+          </form>
           {/* new div */}
           <div className="details-card">
             <h5 className="heading-main">Information</h5>
