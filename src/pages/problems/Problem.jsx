5b0a052b92779ad1037d8d979f3d7177b841b9ac
Danish Sharma <danishsharma13255@gmail.com>
2023-04-27 00:52:25 +0530

    problem page Done. Css Left

--- src/pages/problems/Problem.jsx
+++ src/pages/problems/Problem.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Problem.css";
+import Header from "../../components/header/Header";
+
 const Problem = () => {
-  return <div>hi</div>;
+  const [resp, setResp] = useState([]);
+
+  useEffect(() => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append(
+      "Authorization",
+      "Bearer " + localStorage.getItem("jwtToken")
+    );
+
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+    fetch(
+      process.env.REACT_APP_BASE_URL + "/api/admin/problem/",
+      requestOptions
+    )
+      .then((response) => {
+        response.json().then((value) => {
+          setResp(value);
+        });
+      })
+      .catch((error) => console.log("error", error));
+  }, []);
+  const data = resp.problem || [];
+
+  const Card = ({ problem }) => (
+    <div className="card">
+      <h3 className="card-title">{problem.name}</h3>
+      <div className="card-body">
+        <p className="card-text">Type: {problem.tags}</p>
+        <p className="card-text">Difficulty: {problem.difficulty}</p>
+        <button>Solve Challenge</button>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="main-problems">
+      <div className="problem-header">
+        <Header />
+      </div>
+      <div className="problems-box">
+        <div className="problems-list">
+          {data.map((problem, index) => (
+            <Card key={index} problem={problem} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Problem;
